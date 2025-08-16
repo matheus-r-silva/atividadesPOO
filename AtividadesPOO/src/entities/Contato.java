ee71fd8c8b808ac7dea594a08227a27a590bc6b4
matheus-r-silva <matheus.rocha@ads.uniceplac.edu.br>
2025-08-15 21:56:12 -0300

    Create Contato.java

---
+++ AtividadesPOO/src/entities/Contato.java
@@ -0,0 +1,21 @@
+package entity;
+
+public class Contato {
+	private String nome;
+	private String numero;
+	
+	public Contato() {}
+
+	public Contato(String nome, String numero) {
+		this.nome = nome;
+		this.numero = numero;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+}
