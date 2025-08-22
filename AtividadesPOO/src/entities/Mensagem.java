9cec32212a9f80741be07487c75701f135eded0a
matheus-r-silva <matheus.rocha@ads.uniceplac.edu.br>
2025-08-21 21:51:26 -0300

    Update Mensagem.java

--- AtividadesPOO/src/entities/Mensagem.java
+++ AtividadesPOO/src/entities/Mensagem.java
@@ -4,10 +4,10 @@ import java.util.Scanner;
 
 public class Mensagem {
 	public static Scanner sc = new Scanner(System.in);
-	
+
 	Contato usuario;
 	String texto;
-	
+
 	public Mensagem() {}
 
 	public Mensagem(Contato usuario, String texto) {
@@ -29,23 +29,75 @@ public class Mensagem {
 	}
 	
 	
-	public static void exibirMensagem(Scanner sc) {
+	public static void criarMensagem() {
 		Contato[] usuarios = new Contato[2];
+		Mensagem[] txt = new Mensagem[2];
 		
-		for(int i = 0; i < usuarios.length; i++) {
-			System.out.print("Digite o nome do "+(i + 1)+ " usuario");
-			String remetente = sc.nextLine();
-			System.out.print("Digite o numero do " +(i+1)+" usuario");
+		for(int i = 0; i < 2; i++) {
+			System.out.println("Informe o nome do "+(i+1)+" contato");
+			String nome = sc.nextLine();
+			System.out.println("Informe o numero do "+(i+1)+" contao");
 			String numero = sc.nextLine();
-		
-			usuarios[i] = new Contato(remetente, numero);
-			
+			usuarios[i] = new Contato(nome, numero);
 		}
+		
+		
+		for(int i = 0; i < 2; i++) {
+			System.out.println("Digite a "+(i+1)+" mensagem");
+			String texto = sc.nextLine();
+			txt[i] = new Mensagem(usuarios[i],texto);
+		}
+		
+		for(int i = 0; i < 1; i++) {
+			System.out.println(usuarios[0]+" para "+usuarios[1]+": "+ txt[0]);
+		}
+		System.out.println();
+		
+		for(int i = 2; i < 2; i++) {
+			System.out.println(usuarios[1]+" para "+usuarios[0]+": "+ txt[2]);
+		}
+		
+//		for(Mensagem men : txt) {
+//			System.out.println(men);
+//		}
+		
+//		for(int i = 0; i < 2; i++) {
+//			System.out.println(usuarios[0]+" para "+ usuarios[1]+": "+ txt[i]);
+//		}
+		
 		
 	}
 	
+	
+//	public static void criarUsuario(Scanner sc) {
+//		Contato[] usuarios = new Contato[2];
+//		
+//		for(int i = 0; i < 2; i++) {
+//			System.out.println("Informe o nome do "+(i+1)+" contato");
+//			String nome = sc.nextLine();
+//			System.out.println("Informe o numero do "+(i+1)+" contao");
+//			String numero = sc.nextLine();
+//			usuarios[i] = new Contato(nome, numero);
+//		}
+//		
+//		for(Contato u : usuarios) {
+//			System.out.println(u.getNome()+", ("+ u.getNumero()+")");
+//		}
+//	}
+	
+//	public static void exibirMensagem(Scanner sc) {
+//		System.out.println("Crie os contatos");
+//		criarUsuario(sc);
+//		
+//		Mensagem[] txt = new Mensagem[2];
+//		
+//	}
+	
+	
+	
+
 	public static void main(String[] args) {
-		
+		criarMensagem();
 	}
 //
 //      Método exibirMensagem() que mostre no formato:
