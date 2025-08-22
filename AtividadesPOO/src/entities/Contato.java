7d37fd01099ce506a0e728606c8d10103500e569
IsaAqu <santosaquinoisabela@gmail.com>
2025-08-22 19:48:37 -0300

    Update Contato.java

--- AtividadesPOO/src/entities/Contato.java
+++ AtividadesPOO/src/entities/Contato.java
@@ -1,11 +1,14 @@
 
-
+// Classe Contato
 public class Contato  {
+	// Campos da classe
 	private String nome;
 	private String numero;
-	
+
+	// Construtor Padrão
 	public Contato() {}
 
+	//Construtor
 	public Contato(String nome, String numero) {
 		this.nome = nome;
 		this.numero = numero;
