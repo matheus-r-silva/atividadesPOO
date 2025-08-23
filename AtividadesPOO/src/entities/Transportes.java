e21e3801dbb6c7509f1dff0543036cd19067b4b9
IsaAqu <santosaquinoisabela@gmail.com>
2025-08-22 21:28:48 -0300

    Create Transportes.java

---
+++ AtividadesPOO/src/entities/Transportes.java
@@ -0,0 +1,16 @@
+public class Transporte {
+	
+	private String modelo;
+	
+	public Transporte() {}
+
+	public Transporte(String modelo) {
+		this.modelo = modelo;
+	}
+
+	public String getModelo() {
+		return modelo;
+	}
+	
+	
+}
